58e46d0fdd202ec30a48c811758ab989f98b044f
fps <anthony.nnaemeka.umeh@gmail.com>
2022-03-01 13:18:34 +0100

    Create testing.sol

---
+++ testing.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity > 0.6.0;
+
+import "./calc.sol";
+
+contract nums
+{
+    // math[] matarray;
+    address mataddress;
+
+    constructor()
+    {
+        math newmath = new math();
+        // matarray.push(newmath);
+        mataddress = address(newmath);
+    }
+
+    function perform() public view returns(uint)
+    {
+        // math domath = math(address(matarray[0]));
+
+        //the return value of line 14 is yet unknown but is to be casted into address before use.
+        math domath = math(mataddress);
+        // domath.add([1,2,3,4,5,6]);
+        return domath.sub(11, 2);
+    }
+}
