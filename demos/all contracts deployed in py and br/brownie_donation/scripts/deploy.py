3cec49f61170d39ff8782fd32afd94ed21c1bb73
fps <anthony.nnaemeka.umeh@gmail.com>
2022-03-10 13:46:33 +0100

    Delete demos directory

--- demos/all contracts deployed in py and br/brownie_donation/scripts/deploy.py
+++
@@ -1,9 +0,0 @@
-from brownie import Donation, network, accounts, config
-
-def deploy():
-    account = accounts.add(config['wallet']['from_key'])
-    deploy_var = Donation.deploy({"from": account})
-    print("Deployed")
-
-def main():
-    deploy()
